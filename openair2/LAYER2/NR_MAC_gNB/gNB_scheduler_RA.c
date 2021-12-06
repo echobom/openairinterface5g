45952c7d064fdd99f19d53f2e87cc92e2a6494f0
luis_pereira87 <lpereira@allbesmart.pt>
2021-12-06 14:57:33 +0000

    Fix to support Msg4 DLSCH retransmissions

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -1417,41 +1417,6 @@ void nr_generate_Msg4(module_id_t module_idP, int CC_id, frame_t frameP, sub_fra
 
     NR_BWP_t *genericParameters = bwp ? & bwp->bwp_Common->genericParameters : &scc->downlinkConfigCommon->initialDownlinkBWP->genericParameters;
 
-    // Checking msg4 data
-    uint8_t msg4_data[10240];
-    uint16_t msg4_len = mac_rrc_nr_data_req(module_idP, CC_id, frameP, CCCH, ra->rnti, 1, msg4_data);
-    if(msg4_len==0){
-      LOG_I(NR_MAC,"No msg4, check DCCH message for Reestablishment %x\n",ra->rnti);
-        sched_ctrl->rlc_status[DL_SCH_LCID_DCCH] = mac_rlc_status_ind(module_idP,
-                                                        ra->rnti,
-                                                        module_idP,
-                                                        frameP,
-                                                        slotP,
-                                                        ENB_FLAG_YES,
-                                                        MBMS_FLAG_NO,
-                                                        DL_SCH_LCID_DCCH,
-                                                        0,
-                                                        0);
-        if(sched_ctrl->rlc_status[DL_SCH_LCID_DCCH].bytes_in_buffer>0){
-            msg4_len = mac_rlc_data_req(module_idP,
-                                   ra->rnti,
-                                   module_idP,
-                                   frameP,
-                                   ENB_FLAG_YES,
-                                   MBMS_FLAG_NO,
-                                   DL_SCH_LCID_DCCH,
-                                   sched_ctrl->rlc_status[DL_SCH_LCID_DCCH].bytes_in_buffer,
-                                   (char *)msg4_data,
-                                   0,
-                                   0);
-        }else{
-          LOG_W(NR_MAC, "No Msg4, release ra proc. %x\n", ra->rnti);
-          mac_remove_nr_ue(module_idP, ra->rnti);
-          nr_clear_ra_proc(module_idP, CC_id, frameP, ra);
-          return;
-        }
-    }
-
     long BWPStart = 0;
     long BWPSize = 0;
     NR_Type0_PDCCH_CSS_config_t *type0_PDCCH_CSS_config = NULL;
@@ -1524,10 +1489,46 @@ void nr_generate_Msg4(module_id_t module_idP, int CC_id, frame_t frameP, sub_fra
         // Just send padding LCID
         ra->mac_pdu_length = 0;
       } else {
+        // UE Contention Resolution Identity MAC CE
         uint16_t mac_pdu_length = nr_write_ce_dlsch_pdu(module_idP, nr_mac->sched_ctrlCommon, buf, 255, ra->cont_res_id);
         LOG_D(NR_MAC,"Encoded contention resolution mac_pdu_length %d\n",mac_pdu_length);
-        uint16_t mac_sdu_length = msg4_len;
-        memcpy(&buf[mac_pdu_length+2],msg4_data,msg4_len);
+
+        // Checking Msg4 data (RRCSetup/RRCResume/RRCReestablishment)
+        uint16_t mac_sdu_length = mac_rrc_nr_data_req(module_idP, CC_id, frameP, CCCH, ra->rnti, 1, &buf[mac_pdu_length + sizeof(NR_MAC_SUBHEADER_SHORT)]);
+        if (mac_sdu_length == 0) {
+          LOG_I(NR_MAC,"No Msg4, check DCCH message for RRCReestablishment 0x%0xx\n",ra->rnti);
+          sched_ctrl->rlc_status[DL_SCH_LCID_DCCH] = mac_rlc_status_ind(module_idP,
+                                                                        ra->rnti,
+                                                                        module_idP,
+                                                                        frameP,
+                                                                        slotP,
+                                                                        ENB_FLAG_YES,
+                                                                        MBMS_FLAG_NO,
+                                                                        DL_SCH_LCID_DCCH,
+                                                                        0,
+                                                                        0);
+          if (sched_ctrl->rlc_status[DL_SCH_LCID_DCCH].bytes_in_buffer > 0) {
+            mac_sdu_length = mac_rlc_data_req(module_idP,
+                                        ra->rnti,
+                                        module_idP,
+                                        frameP,
+                                        ENB_FLAG_YES,
+                                        MBMS_FLAG_NO,
+                                        DL_SCH_LCID_DCCH,
+                                        sched_ctrl->rlc_status[DL_SCH_LCID_DCCH].bytes_in_buffer,
+                                        (char *)&buf[mac_pdu_length + sizeof(NR_MAC_SUBHEADER_SHORT)],
+                                        0,
+                                        0);
+          } else {
+            LOG_W(NR_MAC, "No Msg4, release ra proc. 0x%04x\n", ra->rnti);
+            mac_remove_nr_ue(module_idP, ra->rnti);
+            nr_clear_ra_proc(module_idP, CC_id, frameP, ra);
+            return;
+          }
+        }
+
+        AssertFatal(mac_sdu_length<256,"Msg4 with NR_MAC_SUBHEADER_LONG not implemented! Msg4 size: %d\n", mac_sdu_length);
+
         ((NR_MAC_SUBHEADER_SHORT *) &buf[mac_pdu_length])->R = 0;
         ((NR_MAC_SUBHEADER_SHORT *) &buf[mac_pdu_length])->F = 0;
         ((NR_MAC_SUBHEADER_SHORT *) &buf[mac_pdu_length])->LCID = DL_SCH_LCID_CCCH;
