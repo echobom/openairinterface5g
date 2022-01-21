422bf7e10e50420355e57beb418899f3c86557a6
Robert Schmidt <robert.schmidt@openairinterface.org>
2022-01-21 14:40:46 +0100

    MAC stats: group UL&DL common stats

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_uci.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_uci.c
@@ -808,8 +808,8 @@ static void handle_dl_harq(module_id_t mod_id,
     harq->round = 0;
     harq->ndi ^= 1;
     NR_mac_stats_t *stats = &UE_info->mac_stats[UE_id];
-    stats->dlsch_errors++;
-    LOG_D(NR_MAC, "retransmission error for UE %d (total %"PRIu64")\n", UE_id, stats->dlsch_errors);
+    stats->dl.errors++;
+    LOG_D(NR_MAC, "retransmission error for UE %d (total %"PRIu64")\n", UE_id, stats->dl.errors);
   } else {
     add_tail_nr_list(&UE_info->UE_sched_ctrl[UE_id].retrans_dl_harq, harq_pid);
     harq->round++;
