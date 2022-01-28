1e23965bba78c9c748349e484518be7dc20feb9a
luis_pereira87 <lpereira@allbesmart.pt>
2022-01-28 18:10:00 +0000

    Fix lcid in Msg4 for RRCReestablishment case

--- openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
+++ openair2/LAYER2/NR_MAC_gNB/gNB_scheduler_RA.c
@@ -1518,8 +1518,9 @@ void nr_generate_Msg4(module_id_t module_idP, int CC_id, frame_t frameP, sub_fra
 
         // Checking Msg4 data (RRCSetup/RRCResume/RRCReestablishment)
         uint16_t mac_sdu_length = mac_rrc_nr_data_req(module_idP, CC_id, frameP, CCCH, ra->rnti, 1, &buf[mac_pdu_length + sizeof(NR_MAC_SUBHEADER_SHORT)]);
+        uint8_t lcid = DL_SCH_LCID_CCCH;
         if (mac_sdu_length == 0) {
-          LOG_I(NR_MAC,"No Msg4, check DCCH message for RRCReestablishment 0x%0xx\n",ra->rnti);
+          LOG_I(NR_MAC,"No Msg4, check DCCH message for RRCReestablishment 0x%x\n",ra->rnti);
           sched_ctrl->rlc_status[DL_SCH_LCID_DCCH] = mac_rlc_status_ind(module_idP,
                                                                         ra->rnti,
                                                                         module_idP,
@@ -1542,6 +1543,7 @@ void nr_generate_Msg4(module_id_t module_idP, int CC_id, frame_t frameP, sub_fra
                                         (char *)&buf[mac_pdu_length + sizeof(NR_MAC_SUBHEADER_SHORT)],
                                         0,
                                         0);
+            lcid = DL_SCH_LCID_DCCH;
           } else {
             LOG_W(NR_MAC, "No Msg4, release ra proc. 0x%04x\n", ra->rnti);
             mac_remove_nr_ue(module_idP, ra->rnti);
@@ -1554,7 +1556,7 @@ void nr_generate_Msg4(module_id_t module_idP, int CC_id, frame_t frameP, sub_fra
 
         ((NR_MAC_SUBHEADER_SHORT *) &buf[mac_pdu_length])->R = 0;
         ((NR_MAC_SUBHEADER_SHORT *) &buf[mac_pdu_length])->F = 0;
-        ((NR_MAC_SUBHEADER_SHORT *) &buf[mac_pdu_length])->LCID = DL_SCH_LCID_CCCH;
+        ((NR_MAC_SUBHEADER_SHORT *) &buf[mac_pdu_length])->LCID = lcid;
         ((NR_MAC_SUBHEADER_SHORT *) &buf[mac_pdu_length])->L = mac_sdu_length;
         ra->mac_pdu_length = mac_pdu_length + mac_sdu_length + sizeof(NR_MAC_SUBHEADER_SHORT);
         LOG_D(NR_MAC,"Encoded RRCSetup Piggyback (%d + %d bytes), mac_pdu_length %d\n", mac_sdu_length, (int)sizeof(NR_MAC_SUBHEADER_SHORT), ra->mac_pdu_length);
